f0478d2ee13f6c739bef7f1a5759311661c7d22d
MouhsineNejmi <nejmimouhsine@gmail.com>
2025-06-13 16:18:44 +0100

    implemet user update

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,10 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT || '4000'),
   debug: process.env.APP_DEBUG === 'true',
-  appSecret: process.env.APP_SECRET || '',
+  jwtAccessSecret: process.env.JWT_ACCESS_SECRET || '',
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || '',
+  jwtAccessSecretExpiresIn: process.env.JWT_ACCESS_SECRET_EXPIRES_IN || '15m',
+  jwtRefreshSecretExpiresIn: process.env.JWT_REFRESH_SECRET_EXPIRES_IN || '30d',
   issuerBaseUrl: process.env.AUTH0_ISSUER_BASE_URL || '',
   audience: process.env.AUTH0_AUDIENCE || '',
 };
--- src/routes/v1/users/repository.ts
+++ src/routes/v1/users/repository.ts
@@ -1,9 +1,11 @@
 import { Types } from 'mongoose';
 import User, { UserDoc, UserAttrs } from '@/models/user.model';
+import EntityNotFoundError from '@/errors/EntityNotFoundError';
 
 interface IUsersRepository {
   create(attrs: UserAttrs): Promise<UserDoc>;
-  findById(id: Types.ObjectId): Promise<UserDoc | null>;
+  findById(id: string): Promise<UserDoc | null>;
+  update(id: string, updates: Partial<UserDoc>): Promise<UserDoc>;
 }
 
 class UsersRepository implements IUsersRepository {
@@ -11,12 +13,28 @@ class UsersRepository implements IUsersRepository {
     return await User.create(attrs);
   }
 
-  async findById(id: Types.ObjectId): Promise<UserDoc | null> {
-    return await User.findById(id);
+  async findById(id: string): Promise<UserDoc | null> {
+    return await User.findById(new Types.ObjectId(id));
   }
 
   async findByEmail(email: string): Promise<UserDoc | null> {
     return await User.findOne({ email });
+  }
+
+  async update(id: string, updates: Partial<UserDoc>): Promise<UserDoc> {
+    const updatedUser = await User.findByIdAndUpdate(
+      new Types.ObjectId(id),
+      updates,
+      {
+        new: true,
+        runValidators: true,
+      }
+    ).exec();
+
+    if (!updatedUser)
+      throw new EntityNotFoundError({ message: 'User not found' });
+
+    return updatedUser;
   }
 }
 
